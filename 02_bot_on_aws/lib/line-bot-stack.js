dbf28e0430f53188b472bb82dc97fdda2340803c
sumihiro3 <sumihiro@gmail.com>
2023-11-21 18:52:03 +0900

    LineBotMessageLogsTable を追加

--- 02_bot_on_aws/lib/line-bot-stack.js
+++ 02_bot_on_aws/lib/line-bot-stack.js
@@ -1,5 +1,6 @@
 const { Stack, Duration, RemovalPolicy, CfnOutput } = require('aws-cdk-lib');
 const s3 = require('aws-cdk-lib/aws-s3');
+const dynamodb = require('aws-cdk-lib/aws-dynamodb');
 // const sqs = require('aws-cdk-lib/aws-sqs');
 
 class LineBotStack extends Stack {
@@ -14,6 +15,25 @@ class LineBotStack extends Stack {
 
     // The code that defines your stack goes here
 
+    // DynamoDb テーブルの作成
+    const lineBotMessageLogsTable = new dynamodb.Table(this, 'LineBotMessageLogsTable', {
+      tableName: 'LineBotMessageLogs',
+      partitionKey: {
+        name: 'senderId',
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'sentAt',
+        type: dynamodb.AttributeType.NUMBER,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, // オンデマンド
+      removalPolicy: RemovalPolicy.DESTROY, // スタック削除時にテーブルも削除
+    });
+    new CfnOutput(this, 'LineBotMessageLogsTableNameAndArn', {
+      description: 'DynamoDB LineBotMessageLogsTable Name and Arn',
+      value: `${lineBotMessageLogsTable.tableName} : ${lineBotMessageLogsTable.tableArn}`,
+    });
+
     // S3 バケットの作成
     const lineBotContentsBucket = new s3.Bucket(this, 'LineBotContentsBucket', {
       versioned: false,
@@ -22,7 +42,7 @@ class LineBotStack extends Stack {
       autoDeleteObjects: true,
     });
     new CfnOutput(this, 'LineBotContentsBucketNameAndArn', {
-      description: 'S3 LineBotContentsBucket Name and Arn',
+      description: 'DynamoDB LineBotContentsBucket Name and Arn',
       value: `${lineBotContentsBucket.bucketName} : ${lineBotContentsBucket.bucketArn}`,
     });
 
