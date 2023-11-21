94e826453fd149541ab61e9abef8020df0cc57dd
sumihiro3 <sumihiro@gmail.com>
2023-11-21 07:34:31 +0000

    cdk 雛形追加

---
+++ 02_bot_on_aws/test/02_bot_on_aws.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const 02BotOnAws = require('../lib/02_bot_on_aws-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/02_bot_on_aws-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new 02BotOnAws.02BotOnAwsStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
