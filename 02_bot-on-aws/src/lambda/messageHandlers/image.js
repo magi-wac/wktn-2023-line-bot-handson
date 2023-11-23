f4275c91ab90575acb5aee77f1813224ae154591
sumihiro3 <sumihiro@gmail.com>
2023-11-23 19:57:07 +0900

    画像URLの取得に対応

--- 02_bot-on-aws/src/lambda/messageHandlers/image.js
+++ 02_bot-on-aws/src/lambda/messageHandlers/image.js
@@ -5,10 +5,13 @@ import axios from 'axios';
 export const imageMessageHandler = async (event) => {
   console.debug(`imageMessageHandler called!: ${JSON.stringify(event)}`);
   // upload image
-  const location = await uploadImageToS3(event);
+  const uploadResult = await uploadImageToS3(event);
+  console.log(
+    `画像メッセージをS3にアップロードしました: ${JSON.stringify(uploadResult)}`,
+  );
   const replyMessage = {
     type: 'text',
-    text: `画像メッセージを受信しました: ${location}`,
+    text: `画像メッセージを受信しました: ${uploadResult.Location}`,
   };
   return replyMessage;
 };
@@ -20,24 +23,30 @@ export const imageMessageHandler = async (event) => {
 async function uploadImageToS3(event) {
   try {
     const s3Client = new S3Client({});
+    const userId = event.source.userId;
     const messageId = event.message.id;
-    const imageUrl = event.message.contentProvider.originalContentUrl;
+    const imageUrl = getImageUrl(event);
     //
+    if (!imageUrl) {
+      throw new Error('Failed to get image URL');
+    }
     const imageResponse = await axios.get(imageUrl, { responseType: 'stream' });
     const contentType = imageResponse.headers.getContentType();
     const fileExtension = getFileExtensionFromContentType(contentType);
-    const imageFileName = `${messageId}.${fileExtension}`;
+    const imageFileName = `${messageId}.${fileExtension}`; // 画像ファイル名
+    // S3 バケット内の画像ファイルのキー
+    const imageFileKey = `${userId}/images/${imageFileName}`;
     const fileStream = imageResponse.data;
     // upload image to S3
     const upload = new Upload({
       client: s3Client,
       params: {
         Bucket: process.env.LINE_BOT_CONTENTS_BUCKET_NAME,
-        Key: imageFileName,
+        Key: imageFileKey,
         Body: fileStream,
       },
     });
-    console.log(`S3 Bucket へ画像をアップロード中... : ${JSON.stringify(upload)}`);
+    console.debug(`S3 Bucket へ画像をアップロード中... : ${JSON.stringify(upload)}`);
     const uploadResponse = await upload.done();
     if (!(uploadResponse instanceof CompleteMultipartUploadCommandOutput)) {
       throw new Error('Failed to upload image to S3');
@@ -45,7 +54,11 @@ async function uploadImageToS3(event) {
     console.log(
       `S3 Bucket への画像のアップロードが完了しました: ${JSON.stringify(uploadResponse)}`,
     );
-    return uploadResponse.Location;
+    const result = {
+      Location: uploadResponse.Location,
+      Key: uploadResponse.Key,
+    };
+    return result;
   } catch (error) {
     console.error(
       `S3 Bucket への画像のアップロードが失敗しました: ${JSON.stringify(error)}`,
@@ -55,6 +68,24 @@ async function uploadImageToS3(event) {
       console.error(error.stack);
     }
   }
+}
+
+function getImageUrl(event) {
+  const contentProviderType = event.message.contentProvider.type;
+  let imageUrl;
+  switch (contentProviderType) {
+    case 'line':
+      const messageId = event.message.id;
+      imageUrl = `https://api-data.line.me/v2/bot/message/${messageId}/content`;
+      break;
+    case 'external':
+      imageUrl = event.message.contentProvider.originalContentUrl;
+      break;
+    default:
+      imageUrl = null;
+      break;
+  }
+  return contentProviderType.originalContentUrl;
 }
 
 function getFileExtensionFromContentType(contentType) {
